9109e318ba1b31e486b130ccc24de966446c9141
Ian Chagas <ianchagassalgado@hotmail.com>
2023-09-07 12:13:30 -0300

    first project java

---
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,48 @@
+package com.picpaysimplificado.domain.user;
+
+
+import com.picpaysimplificado.dtos.UserDto;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name="users")
+@Table(name="users")
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    public UserType userType;
+
+    public User(UserDto data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.document = data.document();
+        this.email = data.email();
+    }
+}
---
+++ src/main/java/com/picpaysimplificado/dtos/NotificationDto.java
@@ -0,0 +1,4 @@
+package com.picpaysimplificado.dtos;
+
+public record NotificationDto(String email, String message) {
+}
